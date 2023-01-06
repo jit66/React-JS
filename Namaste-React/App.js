bcb3fce0f5cc996032d217c2917b2b62d365bf33
jit66 <jeetthakur251@gmai.com>
2023-01-06 19:54:26 +0530

    Chapter 2- Igniting the App

--- Namaste-React/App.js
+++ Namaste-React/App.js
@@ -1,9 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "h1",
   {
     id: "title",
   },
-  "Heading 1"
+  "Heading 1 for parcel"
 );
 
 const heading2 = React.createElement(
@@ -18,6 +21,7 @@ const container = React.createElement(
   "div",
   {
     id: "container",
+    hello:"world"
   },
   [heading, heading2]
 );
