177820eb6100da92b843aa56fce5df8334293723
jit66 <jeetthakur251@gmai.com>
2023-01-08 02:36:09 +0530

    Chapter 3 - Laying the Foundation

--- Namaste-React/App.js
+++ Namaste-React/App.js
@@ -1,35 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "Heading 1 for parcel"
-);
+// const heading = React.createElement(
+//   "h1",
+//   {                                             // ugly way
+//     id: "title",
+//     key:"h1"
+//   },
+//   "Heading 1 for parcel"
+// );
 
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "title",
-  },
-  "Heading 2"
-);
 
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-    hello:"world"
-  },
-  [heading, heading2]
-);
+const Title = () =>(
+  <h1 id="title" key="h1">Namaste React</h1>
+)
 
-console.log(heading); //element will load up as an object
+const abc = 2
+const HeaderComponent = () => {
+  return (
+    <div>
+       {/* <Title/>  */}
+       {abc}
+      <h1>Hello Functional Component</h1>;
+      <h2>This is a H2 tag</h2>
+    </div>
+    )
+};
+
+console.log(<HeaderComponent/>); //element will load up as an object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+root.render(<HeaderComponent/>);
 
 // document.getElementById("root").innerHTML = "hii"
